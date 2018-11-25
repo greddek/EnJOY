49f7f9bec67e6505954ba135f23964a0099de835
LJ <79zoro@gmail.com>
2018-11-25 18:20:25 +0100

    Level 0

--- Sand.py
+++ Sand.py
@@ -1,1 +1,14 @@
-zmienna = "PSY"
+import pygame
+
+pygame.init()
+
+pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Prosta gra")
+
+window = pygame.display.get_surface()
+window.fill((128, 128, 128))
+
+rectangle = pygame.Surface((80, 20))
+window.blit(rectangle, (100, 50))
+
+pygame.display.flip()
